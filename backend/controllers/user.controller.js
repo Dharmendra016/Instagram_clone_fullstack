87bf964abf8d356c06cdb835d19b86c2ecceaadd
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2024-08-07 07:41:42 +0545

    update on user.controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import {user} from "../models/user.model.js";
+import dotenv from "dotenv"; 
+dotenv.config();
 
 export const register = async(req,res) =>{
     try {
@@ -52,9 +54,79 @@ export const login = async (req, res) => {
             })
         }
 
+        let User = await user.findOne({email});
+        if( !User ){
+            return res.status(401).json({
+                message:"Incorrect email or password",
+                success:false,
+            })
+        }
         
+        const isPasswordMatch = await bcrypt.compare(password , User.password);
+        if( !isPasswordMatch ){
+            return res.status(401).json({
+                message:"Incorrect email or password",
+                success:false,
+            });
+        }
+
+        const UserData = {
+            _id:User._id,
+            username:User.username , 
+            email:User.email,
+            profilePic:User.profilePic , 
+            bio:User.bio, 
+            followers:User.followers,
+            following:User.following, 
+            posts:User.posts,
+        }
+
+        const token = await jwt.sign({UserId:User_id} , process.env.SECRET_KEY , {expireIn:"1d"}); 
+        return res.cookie ('token' , token , {httpOnly:true, sameSite:"strict", maxAge:1*24*60*60*1000}).json({
+            message:`Welcome back ${User.username}`,
+            success:true,
+            UserData,
+        })       
+
     } catch (err) {
         console.log(err.message);
     }
 
+}
+
+export const logout = async (req , res)=>{
+    try {
+        
+        return res.cookie("token" , "" , {maxAge:0}).json({
+            message:"Logged out successfully",
+            success:true,
+        })
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const getProfile = async (req , res) => {
+    try {
+        const userId = req.params.id; 
+        let User =  await user.findById(userId); 
+
+        return res.status(200).json({
+            User, 
+            success:true,
+        })
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+export const exitProfile = async (req ,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error.message);
+    }
 }
