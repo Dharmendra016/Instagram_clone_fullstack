19e2287810b1c34b4c48a6bacb17236c8d06efe9
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2024-09-19 08:56:30 +0545

    adding frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import Signup from "./components/Signup"
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Signup/>
+    </div>
+  )
+}
